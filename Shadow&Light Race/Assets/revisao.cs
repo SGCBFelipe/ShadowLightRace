ff288c2eaf47745735f9d5c54ad39812432552bc
oioi73 <felipelfukunaga@gmail.com>
2025-10-20 15:58:50 -0300

    decoration
    baixei assets packs tirei os que nao precisavam

---
+++ Shadow&Light Race/Assets/revisao.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+public class revisao : MonoBehaviour
+{
+    bool minhaBool = true;
+    int minhaInt = 10;
+    float minhaFloat = 5.5;
+    string minhaString = "Felipe";
+
+    void Start()
+    {
+        Welcome();
+    }
+
+    void Update()
+    {
+
+    }
+    void Welcome()
+    {
+        print("wdadwa");
+    }
+
+    void CalcularIdade(int idade)
+
+        void update
+    {
+        for(int i = 0 i < _Int; i--)
+        {
+        
+        }
+        
+    }
+
+        if (idade >= 18)
+    {
+        print("voce ja e um adulto!")
+    }
+    else
+    {
+        print("voce é uma criança")
+    }
+    
+}
